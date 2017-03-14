dfdbb057a246edcfcfc3dc79fb4127970d82d08b
Jason Hamilton <me@jason.sx>
2017-03-13 23:56:38 -0400

    Cleaned up map controls

--- js/map.js
+++ js/map.js
@@ -61,6 +61,7 @@ new Maplace({
     start: 0,
     map_options: {
         zoom: 4,
+        disableDefaultUI: true,
         mapTypeId: 'terrain',
         styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.country","elementType":"geometry.fill","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#e10011"},{"visibility":"on"}]}]
     }
