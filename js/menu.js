ec2f4c879df5a1b4a8e9786e2ae57a900e15a85f
Jason Hamilton <me@jason.sx>
2017-09-27 23:04:20 -0400

    Fixed Menu / Logo issue

--- js/menu.js
+++ js/menu.js
@@ -3,10 +3,12 @@ $('#ham-toggle').click(function() {
   	$('#ham-toggle i').attr("class", "fa fa-times");
   	$('#menu').css('display','block');
   	$('#logo img').attr("class", "menu-open");
+    $('#article-page').attr("class", "open_page");
   } else {
   	$('#ham-toggle i').attr("class", "fa fa-bars");
   	$('#menu').css('display','none');
   	$('#banner').css('margin-top','0');
   	$('#logo img').attr("class", "logo");
+    $('#article-page').attr("class", "closed");
   }
 });
